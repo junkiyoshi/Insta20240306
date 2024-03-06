653690d0ba80918138232fc8c229402246377fe8
NAKAUCHI Kiyoshi <junkiyoshi@gmail.com>
2024-03-06 21:20:26 +0900

    Add files via upload

---
+++ ofApp.cpp
@@ -0,0 +1,95 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup() {
+
+	ofSetFrameRate(25);
+	ofSetWindowTitle("openFrameworks");
+
+	ofBackground(239);
+}
+
+//--------------------------------------------------------------
+void ofApp::update() {
+
+	ofSeedRandom(39);
+}
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+
+	for (int x = 20; x <= ofGetWidth(); x += 40) {
+
+		auto y = ofMap(ofNoise(x * 0.03, ofGetFrameNum() * 0.003), 0, 1, 45, ofGetHeight()) - 45;
+
+		this->draw_arrow(glm::vec2(x, ofGetHeight()), glm::vec2(x, y + 60), 20, ofColor(0));
+		this->draw_arrow(glm::vec2(x, 0), glm::vec2(x, y - 60), 20, ofColor(0));
+	}
+
+	/*
+	int start = 50;
+	if (ofGetFrameNum() > start) {
+
+		ostringstream os;
+		os << setw(4) << setfill('0') << ofGetFrameNum() - start;
+		ofImage image;
+		image.grabScreen(0, 0, ofGetWidth(), ofGetHeight());
+		image.saveImage("image/cap/img_" + os.str() + ".jpg");
+		if (ofGetFrameNum() - start >= 25 * 20) {
+
+			std::exit(1);
+		}
+	}
+	*/
+}
+
+//--------------------------------------------------------------
+void ofApp::draw_arrow(glm::vec2 location, glm::vec2 target, float size, ofColor color) {
+
+	auto angle = std::atan2(target.y - location.y, target.x - location.x);
+	auto distance = glm::distance(target, location);
+
+	ofPushMatrix();
+	ofTranslate(target);
+
+	ofSetColor(color);
+	ofFill();
+	ofBeginShape();
+	ofVertex(glm::vec2(size * cos(angle), size * sin(angle)));
+	ofVertex(glm::vec2(size * 0.5 * cos(angle + PI * 0.5), size * 0.5 * sin(angle + PI * 0.5)));
+	ofVertex(glm::vec2(size * 0.5 * cos(angle - PI * 0.5), size * 0.5 * sin(angle - PI * 0.5)));
+	ofEndShape();
+
+	ofBeginShape();
+	ofVertex(glm::vec2(size * 0.5 * cos(angle + PI * 0.5), size * 0.5 * sin(angle + PI * 0.5)) * 0.5);
+	ofVertex(glm::vec2(size * 0.5 * cos(angle + PI * 0.5), size * 0.5 * sin(angle + PI * 0.5)) * 0.5 - glm::vec2(distance * cos(angle), distance * sin(angle)));
+	ofVertex(glm::vec2(size * 0.5 * cos(angle - PI * 0.5), size * 0.5 * sin(angle - PI * 0.5)) * 0.5 - glm::vec2(distance * cos(angle), distance * sin(angle)));
+	ofVertex(glm::vec2(size * 0.5 * cos(angle - PI * 0.5), size * 0.5 * sin(angle - PI * 0.5)) * 0.5);
+	ofEndShape();
+
+	ofSetColor(255);
+	ofNoFill();
+	ofBeginShape();
+	ofVertex(glm::vec2(size * 0.5 * cos(angle + PI * 0.5), size * 0.5 * sin(angle + PI * 0.5)) * 0.5);
+	ofVertex(glm::vec2(size * 0.5 * cos(angle + PI * 0.5), size * 0.5 * sin(angle + PI * 0.5)));
+	ofVertex(glm::vec2(size * cos(angle), size * sin(angle)));
+	ofVertex(glm::vec2(size * 0.5 * cos(angle - PI * 0.5), size * 0.5 * sin(angle - PI * 0.5)));
+	ofVertex(glm::vec2(size * 0.5 * cos(angle - PI * 0.5), size * 0.5 * sin(angle - PI * 0.5)) * 0.5);
+	ofEndShape();
+
+	ofBeginShape();
+	ofVertex(glm::vec2(size * 0.5 * cos(angle - PI * 0.5), size * 0.5 * sin(angle - PI * 0.5)) * 0.5);
+	ofVertex(glm::vec2(size * 0.5 * cos(angle - PI * 0.5), size * 0.5 * sin(angle - PI * 0.5)) * 0.5 - glm::vec2(distance * cos(angle), distance * sin(angle)));
+	ofVertex(glm::vec2(size * 0.5 * cos(angle + PI * 0.5), size * 0.5 * sin(angle + PI * 0.5)) * 0.5 - glm::vec2(distance * cos(angle), distance * sin(angle)));
+	ofVertex(glm::vec2(size * 0.5 * cos(angle + PI * 0.5), size * 0.5 * sin(angle + PI * 0.5)) * 0.5);
+	ofEndShape();
+
+	ofPopMatrix();
+}
+
+//--------------------------------------------------------------
+int main() {
+
+	ofSetupOpenGL(720, 720, OF_WINDOW);
+	ofRunApp(new ofApp());
+}
